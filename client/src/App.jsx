2da3f88a1eb6e6744b293e50c446af1895448a6a
Krishna Thakkar <work.krishnat@gmail.com>
2024-04-13 21:31:23 +0530

    Created Home Page and Header Icon Modals (dialogs)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,42 @@
-import React, { lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import { LayoutLoader } from "./components/layout/Loaders";
 
 const Home = lazy(() => import("./pages/Home"));
-const Login  = lazy(() => import("./pages/Login"));
-const Chat  = lazy(() => import("./pages/Chat"));
-const Groups  = lazy(() => import("./pages/Groups"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = true;
 
 function App() {
   return (
     <>
       <Router>
+        <Suspense fallback={<LayoutLoader />} >
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/groups" element={<Groups />} />
-          
+          <Route path="/" element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        </Suspense>
       </Router>
     </>
   );
--- client/src/components/styles/StyledComponents.jsx
+++ client/src/components/styles/StyledComponents.jsx
@@ -1,4 +1,5 @@
 import { styled } from "@mui/material";
+import { Link as LinkComponent } from "react-router-dom";
 
 const VisuallyHiddenInput = styled("input")({
   border: 0,
@@ -12,4 +13,13 @@ const VisuallyHiddenInput = styled("input")({
   width: 1,
 });
 
-export { VisuallyHiddenInput };
+const Link = styled(LinkComponent)`
+  text-decoration: none;
+  color: black;
+  padding: 1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export { VisuallyHiddenInput,Link };
